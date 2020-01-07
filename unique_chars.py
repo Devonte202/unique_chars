50584f12b2766591f00e5288c480d55a0b6c2966
EC2 Default User <ec2-user@ip-172-31-16-222.us-east-2.compute.internal>
2020-01-07 15:53:26 +0000

    code challenge

---
+++ unique_chars.py
@@ -0,0 +1,33 @@
+"""
+Problem: Go through string to see if any characters are repeated
+input: string
+output: boolean
+Examples: 
+uniqueChars('ABC') == true
+uniqueChars('AAB') == false
+uniqueChars('Aa') == true
+uniqueChars('') == None
+
+Data Structure: Dictionary & Lists
+Algorithm: 
+0: If input length is 0 then return None
+1: Check to see if character is in dictionary
+    I: if not store in string into a dictionary as properties with a value of 1
+    II: If character already lives on dictionary, increment property value
+    
+2: Loop through dictionary and check to see if any values are greater than 1
+3: If any values are greater than 1 return false, otherwise return true
+"""
+
+def uniqueChars(string):
+    if(len(string) == 0):
+        return None
+    charCount = {}
+    for char in string:
+        if char in charCount:
+            charCount.char += 1
+        else charCount.char = 1
+    for char in charCount:
+        if len(char) > 1:
+            return True
+    return False
